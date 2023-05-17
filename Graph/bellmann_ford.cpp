360359383ec50d95c32c9f8cd11b42f159e87b06
Sanjay <codecheffsanjay@gmail.com>
2023-05-17 14:37:54 +0530

    update bellmann ford

--- Graph/bellmann_ford.cpp
+++ Graph/bellmann_ford.cpp
@@ -6,6 +6,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// It should be noted that toSWhen running the Bellman-Ford algorithm with the point as the source point, if there is no negative cycle result, it can only be explained fromSStarting from a point cannot reach a negative cycle, but it cannot explain that there is no negative cycle on the graph.
+// Therefore, if it is necessary to determine whether there is a negative cycle on the entire graph, the most rigorous approach is to establish a super source point, connect an edge with a weight of 0 to each node on the graph, and then execute the Bellman-Ford algorithm starting from the super source point .
+
 #define pb push_back 
 struct node{
     int a;
@@ -31,12 +34,16 @@ int32_t main(){
         graph.pb((node(a,b,w)));
     }
 
+    int source = 1;
     vector<int> d(n+1,INT_MAX);
-    d[1]=0;
+    d[source]=0;
     bool x;
     for(int i=1 ; i<=n ; i++){
         x = true;
         for(auto it:graph){
+            if(d[it.a]==INT_MAX){
+                continue;
+            }
             // if change doen't happen than u break 
             if(d[it.b] > d[it.a]+it.w){
                 d[it.b] =  d[it.a] + it.w ;
